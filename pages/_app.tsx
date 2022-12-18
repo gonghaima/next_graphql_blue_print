b257e50527aca17d30c337fda13efd98afff41b0
Steven Gong <gonghaima@hotmail.com>
2022-12-18 21:02:33 +1100

    add layout components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { Hydrate, QueryClientProvider } from 'react-query';
 import type { AppProps } from 'next/app';
 import { queryClient } from '../src/api';
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </Hydrate>
     </QueryClientProvider>
   );
